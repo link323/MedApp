45dfff16f90e166021932269f182452741d6df87
ewelina <ewelina.hojdys@gmail.com>
2016-04-15 19:58:56 +0200

    added new tables

--- app/src/main/java/com/example/pc/medproject/DataBaseHelper.java
+++ app/src/main/java/com/example/pc/medproject/DataBaseHelper.java
@@ -14,12 +14,25 @@ public class DataBaseHelper extends SQLiteOpenHelper
 	}
 	// Called when no database exists in disk and the helper class needs
 	// to create a new one.
+
+	@Override
+	public void onOpen(SQLiteDatabase db) {
+		super.onOpen(db);
+		if (!db.isReadOnly()) {
+			// Enable foreign key constraints
+			db.execSQL("PRAGMA foreign_keys=ON;");
+		}
+	}
+
 	@Override
 	public void onCreate(SQLiteDatabase _db) 
 	{
-			_db.execSQL(LoginDataBaseAdapter.DATABASE_CREATE);
+			_db.execSQL(DataBaseAdapter.CREATE_USER_TABLE);
+			_db.execSQL(DataBaseAdapter.CREATE_RESULTS_TABLE_1);
+			_db.execSQL(DataBaseAdapter.CREATE_RESULTS_TABLE_2);
 			
 	}
+
 	// Called when there is a database version mismatch meaning that the version
 	// of the database on disk needs to be upgraded to the current version.
 	@Override
